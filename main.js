fe71d9c66aceb8e45132b0c144e75e16784e71aa
avikj <avikjain12345@gmail.com>
2016-02-21 22:26:36 -0800

    Initial commit

--- main.js
+++ main.js
@@ -1,80 +1,84 @@
 var synaptic = require('synaptic');
-
-var Layer = synaptic.Layer,
-    Network = synaptic.Network;
-
 var fs = require('fs');
-fs.readFile('training_data.txt','utf8',function(err, data){
+var Layer = synaptic.Layer,
+    Network = synaptic.Network,
+    Trainer = synaptic.Trainer;
+
+
+// set up neural network
+var inputLayer = new Layer(4);
+var hiddenLayer = new Layer(5);
+var outputLayer = new Layer(3);
+
+inputLayer.project(hiddenLayer);
+hiddenLayer.project(outputLayer);
+
+var network = new Network({
+    input: inputLayer,
+    hidden: [hiddenLayer],
+    output: outputLayer
+});
+
+
+
+// read training data file
+fs.readFile('iris.txt','utf8',function(err, data){
 	if(err) throw err;
 
-	// read data from training data file and set up training data array
+	// load data into training data array
 	var lines = data.split("\n");
-	var training_data = [];
+	var trainingData = [];
 	for(var i = 0; i < lines.length; i++){
 		var line = lines[i].trim();
-		var split_line = line.split(",");
-		var data_point = {};
-		var input = split_line.slice(0, 4);
+		var splitLine = line.split(",");
+		var input = splitLine.slice(0, 4);
 
-		var output = split_line[4]=='Iris-virginica' ? [0,0,1] 
-					: split_line[4] == 'Iris-versicolor' ? [0,1,0] 
+		var output = splitLine[4]=='Iris-virginica' ? [0,0,1] 
+					: splitLine[4] == 'Iris-versicolor' ? [0,1,0] 
 					: [1,0,0];
 
-		training_data.push({
+		trainingData.push({
 			input: input,
 			output: output
 		});
 	}
 
-	//set up neural network
-	var inputLayer = new Layer(4);
-	var hiddenLayer = new Layer(3);
-	var outputLayer = new Layer(3);
-
-	inputLayer.project(hiddenLayer);
-	hiddenLayer.project(outputLayer);
-
-	var network = new Network({
-	    input: inputLayer,
-	    hidden: [hiddenLayer],
-	    output: outputLayer
-	});
-
-
 	// train the network
-	var learningRate = .3;
-	for (var i = 0; i < 1000; i++)
+	var learningRate = .01;
+	for (var i = 1; i <= 10000; i++)
 	{
-		for(var j = training_data.length-1; j >= 0; j--){
-			network.activate(training_data[j].input);
-			network.propagate(learningRate, training_data[j].output);
+		for(var j = 0; j < trainingData.length; j++){
+			network.activate(trainingData[j].input);
+			network.propagate(learningRate, trainingData[j].output);
 		}
 		if(i%1000 == 0)
-			console.log(i/100+"% done.");
+			console.log("Training... "+i/100+"% complete. ");
 	}
-
-
-	fs.readFile('training_data.txt','utf8',function(err, data){
-		if(err) throw err;
-			
-		var lines = data.split("\n");
-
-		for(var i = 0; i < lines.length; i++){
-			var line = lines[i].trim();
-			var split_line = line.split(",");
-			var data_point = {};
-			var input = split_line.slice(0, 4);
-
-			var output = split_line[4]=='Iris-virginica' ? [0,0,1] 
-						: split_line[4] == 'Iris-versicolor' ? [0,1,0] 
-						: [1,0,0];
-
-			testNetwork(network, input, output);
-		}
-	});
-	
 });
 
-function testNetwork(network, inputs, expectedOutput){
-	console.log("output:"+network.activate(inputs)+"\nshould be:"+expectedOutput+"\n\n");
+fs.readFile('testing_data.txt','utf8',function(err, data){
+	if(err) throw err;
+	console.log("\n\nResults\n======================================\n");
+	var lines = data.split("\n");
+	for(var i = 0; i < lines.length; i++){
+		var input = lines[i].trim().split(",");
+		var result = getFlowerName(network.activate(input));
+		console.log(lines[i].trim()+" => "+result);
+	}
+});
+function getLargestIndex(arr){
+	var result = 0;
+	for(var i = 1; i < arr.length; i++)
+		if(arr[i] > arr[result])
+			result = i;
+	return result;
+}
+
+function getFlowerName(arr){
+	var index = getLargestIndex(arr);
+	if(index == 0)
+		return "Iris-setosa";
+	if(index == 1)
+		return "Iris-versicolor";
+	return "Iris-virginica"
 }
